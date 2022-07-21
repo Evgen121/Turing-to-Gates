cb7a4924a3e83bb6f6d9584dda0ffea887b71635
Yevhen Malyi <malyje164@gmail.com>
2022-07-21 21:32:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ $('#f1').click(function() {
+     $('#f1').attr('src', 'image/1b.jpg');
+ });
+ $('#f2').click(function() {
+     $('#f2').attr('src', 'image/2b.jpg');
+ });
+ $('#f3').click(function() {
+     $('#f3').attr('src', 'image/3b.jpg');
+ });
